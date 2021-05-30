cd24fe62f5e75d315140add920cb7d6f57edbe60
Martin Debray <martin@debray.studio>
2021-05-30 13:35:29 +0200

    Ekip ekip

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,36 @@
 chrome.extension.sendMessage({}, function (response) {
-  if (/Edge/.test(navigator.userAgent)) {
-    alert('Déso Nico, faut un vrai browser par contre!')
-  }
-
   var readyStateCheckInterval = setInterval(function () {
     if (document.readyState === 'complete') {
       clearInterval(readyStateCheckInterval)
-      console.log('Wesh alors')
+      masterPawner()
+
+      console.log(
+        'Find out more over there http://djw5nmsmjljcf5reiugq3irwitfo7nx2krjmrxslyhrzfxcspebmotqd.onion'
+      )
     }
   }, 10)
 })
+
+function masterPawner () {
+  ripSatya()
+
+  if (window.location.origin.indexOf('lequipe.fr') > -1) {
+    ekipekip()
+  }
+}
+
+function ripSatya () {
+  if (/Edge/.test(navigator.userAgent)) {
+    alert('Déso Nico, faut un vrai browser par contre!')
+  }
+}
+
+function ekipekip () {
+  window.addEventListener(
+    'scroll',
+    function (event) {
+      event.stopImmediatePropagation()
+    },
+    true
+  )
+}
